8eb44851e3ccb5bae9ab91939ad1a32f8691f806
yigit123 <151040519+yigitefebay88@users.noreply.github.com>
2026-09-08 14:03:36 +0300

    COMMİT ALINDI

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("com.android.application") version "8.5.2" apply false
     id("com.android.library") version "8.5.2" apply false
     id("com.android.test") version "8.5.2" apply false
+    id("org.jetbrains.kotlin.multiplatform") version "2.1.0" apply false
     id("org.jetbrains.kotlin.android") version "2.1.0" apply false
     id("org.jetbrains.kotlin.jvm") version "2.1.0" apply false
     id("com.google.dagger.hilt.android") version "2.57.1" apply false
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -24,6 +24,7 @@ dependencyResolutionManagement {
 
 rootProject.name = "FocusPath"
 include(":app")
+include(":shared")
 include(":core:domain")
 include(":core:data")
 include(":baselineprofile")
