bc9061e5911c081eeb7757823eacc7089071bdee
yigit123 <151040519+yigitefebay88@users.noreply.github.com>
2026-09-17 11:29:41 +0300

    düzenlendi

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,7 +27,7 @@ android {
         applicationId = "com.focuspath.app"
         minSdk = 24
         targetSdk = 36
-        versionCode = 109
+        versionCode = 110
         versionName = "1.2.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
